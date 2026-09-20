03e1c5a10f9f6e5700f981ed953c00e9aa324832
EmilyDoesStuff <emilyrichardson200298@gmail.com>
2026-09-20 19:40:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (!page) return;
 
+    const stickers = [
+        "🦋",
+        "🌸",
+        "🌷",
+        "🪻",
+        "🌼",
+        "🌺",
+        "💗",
+        "💕",
+        "💞",
+        "✨",
+        "💫",
+        "🪽",
+        "🌙",
+        "☁️",
+        "🌿",
+        "🍓",
+        "🍒",
+        "🧚"
+    ];
+
+    const frontSticker = page.querySelector(".front .sticker");
+    const backSticker = page.querySelector(".back .sticker");
+
+    let stickerNumber = 0;
+
     page.addEventListener("click", function () {
+
+        stickerNumber++;
+
+        if (frontSticker) {
+            frontSticker.textContent =
+                stickers[stickerNumber % stickers.length];
+        }
+
+        if (backSticker) {
+            backSticker.textContent =
+                stickers[(stickerNumber + 1) % stickers.length];
+        }
 
         page.classList.toggle("flipped");
 
